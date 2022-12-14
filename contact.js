7c5433ea167eaaad1f3467184c191c5f530b4048
mila <81945866+oriflaimmm@users.noreply.github.com>
2022-12-14 10:56:22 +0300

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // это в контактах как связаться 
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Это обязательное поле",
+                    minlength: "Имя должно состоять как минимум из 2 символов"
+                },
+                subject: {
+                    required: "Это обязательное поле",
+                    minlength: "Тема должна состоять как минимум из 4 символов"
+                },
+                number: {
+                    required: "Это обязательное поле",
+                    minlength: "Номер должен состоять из 10 символов"
+                },
+                email: {
+                    required: "Введите ваш Email"
+                },
+                message: {
+                    required: "Это обязательное поле",
+                    minlength: "Пожалуйста, опишите проблему подробнее"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
