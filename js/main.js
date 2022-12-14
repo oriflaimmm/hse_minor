00482bb80ed2cf64fe7b96efc188a720abe13390
mila <81945866+oriflaimmm@users.noreply.github.com>
2022-12-14 10:52:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+$(function() {
+  "use strict";
+
+// выбор
+  $('select').niceSelect();
+
+  // карусель в выборе товара 
+  $(".hero-carousel").owlCarousel({
+    items:3,
+    margin: 10,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items: 2
+      },
+      810:{
+        items:3
+      }
+    }
+  });
+
+// карусель топ товары
+  if($('.owl-carousel').length > 0){
+    $('#bestSellerCarousel').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:true,
+      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+      dots: false,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items: 2
+        },
+        900:{
+          items:3
+        },
+        1130:{
+          items:4
+        }
+      }
+    })
+  }
+
+// карусель
+  $(".s_Product_carousel").owlCarousel({
+    items:1,
+    autoplay:false,
+    autoplayTimeout: 5000,
+    loop:true,
+    nav:false,
+    dots:false
+  });
+
+// для почты
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+  mailChimp();
+  
+// навигационная панель 
+  $(window).scroll(function(){
+    var sticky = $('.header_area'),
+    scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
+
+// ползунок для цены
+  if(document.getElementById("price-range")){
+  
+    var nonLinearSlider = document.getElementById('price-range');
+    
+    noUiSlider.create(nonLinearSlider, {
+        connect: true,
+        behaviour: 'tap',
+        start: [ 100, 200000 ],
+        range: {
+            // от 100 до 10000 шаг 500,
+            // от 10000 до 200000 шаг 1000.
+            'min': [ 0 ],
+            '10%': [ 500, 500 ],
+            '50%': [ 10000, 1000 ],
+            'max': [ 200000 ]
+        }
+    });
+  
+  
+    var nodes = [
+        document.getElementById('lower-value'), // 0
+        document.getElementById('upper-value')  // 1
+    ];
+  
+    // отображение ползунка и как далеко он отодвигается от левого края
+    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
+        nodes[handle].innerHTML = values[handle];
+    });
+  
+  }
+  
+});
+
+
